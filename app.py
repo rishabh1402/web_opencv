7dee376ba81a6df246d94b838d123ecd7f9b361b
Rishabh Rajput <75234829+rishabh1402@users.noreply.github.com>
2022-04-16 11:28:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from sys import stdout
-from process import webopencv
 import logging
 from flask import Flask, render_template, Response, request, jsonify
 from flask_socketio import SocketIO
 from camera import Camera
-from utils import base64_to_pil_image, pil_image_to_base64
-# import jsonify
+
 
 
 #----------------- Video Transmission ------------------------------#
@@ -13,7 +11,7 @@ app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(stdout))
 app.config['DEBUG'] = True
 socketio = SocketIO(app)
-camera = Camera(webopencv())
+camera = Camera()
 
 #---------------- Video Transmission --------------------------------#
 
@@ -37,12 +35,12 @@ def index():
     return render_template('index.html')
 
 
-def gen():
+def gen(Camera,no):
     """Video streaming generator function."""
 
     app.logger.info("starting to generate frames!")
     while True:
-        frame = camera.get_frame() #pil_image_to_base64(camera.get_frame())
+        frame = Camera.get_frame()[no] #pil_image_to_base64(camera.get_frame())
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
@@ -50,7 +48,8 @@ def gen():
 @app.route('/video_feed')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    frame = gen(camera,0)
+    return Response(frame, mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
